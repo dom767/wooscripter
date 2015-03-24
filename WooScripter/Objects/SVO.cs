e41dbef512510f92d55d0433a33ea7286393e980
dom767 <dominicpenfold@gmail.com>
2015-03-24 22:56:09 +0000

    Sparse Voxel Octree
    Simple renderer for sparse voxel octrees

---
+++ WooScripter/Objects/SVO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace WooScripter.Objects.WooScript
+{
+    public class SVO : RenderObject
+    {
+        public Vector3 _Position;
+        public Vector3 _Scale;
+        public Matrix3 _Rotation;
+        public Material _Material;
+        public string _DistanceFunction;
+        public double _DistanceMinimum;
+        public double _DistanceScale;
+        public Vector3 _DistanceOffset;
+        public double _StepSize;
+        public int _Depth;
+
+        public SVO(Vector3 centre,
+            Vector3 scale,
+            Matrix3 rotation,
+            string distanceFunction,
+            double distanceMinimum,
+            double distanceScale,
+            Vector3 distanceOffset,
+            double stepSize,
+            int depth)
+        {
+            _Material = new Material();
+            _Position = new Vector3();
+            _Scale = new Vector3();
+            _Rotation = new Matrix3();
+            _Position = centre;
+            _Scale = scale;
+            _Rotation = rotation;
+            _DistanceFunction = distanceFunction;
+            _DistanceMinimum = distanceMinimum;
+            _DistanceScale = distanceScale;
+            _DistanceOffset = distanceOffset;
+            _StepSize = stepSize;
+            _Depth = depth;
+        }
+
+        public void CreateElement(bool preview, XElement parent)
+        {
+            XElement ret = new XElement("OBJECT",
+                new XAttribute("type", 7),
+                new XAttribute("position", _Position),
+                new XAttribute("scale", _Scale),
+                new XAttribute("rotation", _Rotation),
+                new XAttribute("ignoreWhileLighting", false),
+                new XAttribute("distancefunction", _DistanceFunction),
+                new XAttribute("minimumdistance", _DistanceMinimum),
+                new XAttribute("distancescale", _DistanceScale),
+                new XAttribute("distanceoffset", _DistanceOffset),
+                new XAttribute("stepsize", _StepSize),
+                new XAttribute("depth", _Depth),
+                _Material.CreateElement(preview));
+            parent.Add(ret);
+        }
+    }
+}
---
+++ WooScripter/Objects/WooScript/SVORule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WooScripter.Objects.WooScript
+{
+    class SVORule : Rule
+    {
+        public SVORule(string name)
+            : base(name)
+        {
+        }
+
+        public override string GetHelpText()
+        {
+            return "svo - Create a sparse voxel octree";
+        }
+
+        public override bool CanRecurse()
+        {
+            return false;
+        }
+
+        public override void Execute(ref WooState state)
+        {
+            Vector3 val = new Vector3(0.0, 0.5, 0.0);
+            val.y *= state._Scale.y;
+            val.Mul(state._Rotation);
+
+            SVO newSVO = new SVO(new Vector3(state._Position.x + val.x, state._Position.y + val.y, state._Position.z + val.z),
+                state._Scale,
+                state._Rotation,
+                state._DistanceFunction,
+                state._DistanceMinimum,
+                state._DistanceScale,
+                state._DistanceOffset,
+                state._StepSize,
+                state._Depth);
+            newSVO._Material = GenerateMaterial(state);
+            newSVO.CreateElement(state._Preview, state._Parent);
+        }
+    }
+}
